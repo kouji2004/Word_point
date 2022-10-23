f630b61aee0da4f517a19acb228f0a8df77e1857
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-23 20:15:40 +0900

    クラス（設計図）インスタンス（実態）

--- java_code/Human.java
+++ java_code/Human.java
@@ -2,14 +2,25 @@ package java_code;
 
 public class Human {
   public static void main(String[] args) {
-    Humans sas = new Humans();
-    sas.name = "miura";
-    System.out.println(sas.name);
+    // Aクラスの人
+    Humans Aclass = new Humans();
+    System.out.println("\t");
+    System.out.println("Aクラスの生徒");
+    System.out.println(Aclass.Aname);
+    Aclass.calculateAvg(80, 90);
+
+    // Bクラスの人
+    Humans Bclass = new Humans();
+    System.out.println("\t");
+    System.out.println("Bクラスの生徒");
+    System.out.println(Bclass.Bname);
+    Bclass.calculateAvg(70, 50);
   }
 }
 
 class Humans {
-  String name = "miura";
+  String Aname = "miura";
+  String Bname = "tarou";
 
   public void calculateAvg(int math, int english) {
     System.out.println((math + english) / 2);
